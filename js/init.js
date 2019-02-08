f5eb35f0c5a27e654db821e741ed77ac7ebfe92f
danfred360 <dannyfrederick360@gmail.com>
2019-02-07 22:02:53 -0500

    init

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.collapsible').collapsible();
+    $('.fixed-action-btn').floatingActionButton();
+    $('.slider').slider();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
